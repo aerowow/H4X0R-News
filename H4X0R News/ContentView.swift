7f012b956ad252754feef564ae072133a09bb456
Daniil Kiryanchuk <job.kiryanchuk@gmail.com>
2022-06-18 19:04:50 +0300

    Observer Design Pattern

--- H4X0R News/ContentView.swift
+++ H4X0R News/ContentView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
     var body: some View {
         NavigationView{
-            List(posts) { post in
-                Text(post.title)
+            List(networkManager.posts) { post in
+                HStack {
+                    Text(String(post.points))
+                    Text(post.title)
+                }
             }
             .navigationTitle("H4X0R NEWS")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
         }
     }
 }
@@ -26,8 +35,8 @@ struct ContentView_Previews: PreviewProvider {
 
 
 
-let posts = [
-Post(id: "1", title: "Hello"),
-Post(id: "2", title: "Bonjour"),
-Post(id: "3", title: "Privet")
-]
+//let posts = [
+//Post(id: "1", title: "Hello"),
+//Post(id: "2", title: "Bonjour"),
+//Post(id: "3", title: "Privet")
+//]
